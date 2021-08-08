bbc91bf653a4175b5c87f5d4297d1ce5834d1c84
godgametv <80158977+godgametv@users.noreply.github.com>
2021-08-08 10:50:31 +0700

    Create q.py

---
+++ q.py
@@ -0,0 +1,57 @@
+import os 
+os.system("clear")
+import time
+time.sleep(2.0)
+
+print("")
+print("")
+
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+
+mengetik('>> Username: Arimengen\n>> Password: **********************')
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+mengetik('\x1b[6;30;42m' + 'Halo selamat datang di Termux, Jangan lupa Subscribe NinjaSarungTV\n#\n#\n#\n#')
+print('\x1b[6;30;42m' + 'Running script!')
+
+import webbrowser
+
+url('https://www.youtube.com/channel/UCiPq2EJZ5ZXaZTjAbJ4FOTw')
+webbrowser.open_new(url)
+
+
+import os
+os.system("pkg install php")
+os.system('pkg install cmatrix')
+os.system('pkg install python2')
+os.system("clear")
+os.system("python b.py")
+
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+
+        time.sleep(random.random() * 0.1)
+
+mengetik('>> Running Script')
+
+time.sleep(0.9)
+print("DONE")
+
+time.sleep(0.6)
+os.system("clear")
+print("start script (php rhawk.php)")
